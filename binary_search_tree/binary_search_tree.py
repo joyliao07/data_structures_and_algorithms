7396b1cde0326cef9132080bacb35b697ec4cb0b
Joyce Liao <joyliao07@gmail.com>
2018-12-14 22:07:26 -0800

    working add and insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -19,7 +19,6 @@ class BST(object):
     """To create a node(s) for a binary search tree and its related methods."""
 
     def __init__(self, iterable=None):
-        # self.front = None
         self.root = None
         self._size = 0
 
@@ -30,7 +29,7 @@ class BST(object):
             raise TypeError('Iterable is not a list.')
 
         for what in iterable:
-            self.add(what)
+            self.add_node(what)
 
     def __str__(self):
         output = f'Value of the root Queue is: {self.root.val}'
@@ -42,57 +41,65 @@ class BST(object):
     def __repr__(self):
         return f'<BST root: { self.root.val }>'
 
-    def add(self, val):
+    def prep(self, val):
+        pass
+
+    def add_node(self, val):
         """
         """
+        print('line 50 start to create new node: ', val)
         if self.root is None:
-            new_node = Node(val)
-            self.root = new_node
-            print('self.root is added: ', self.root.val)
-            self._size += 1
-            return self
-        
-        else:
-            new_node = Node(val)
-            self._size += 1
+            self.root = Node(val)
+            print('root is created outside insersion: ', self.root.val)
+            return
 
-            current = self.root
-            print('add new_node below root level: ', new_node.val)
-
-            while current:
-                print('inside while loop, new_node.val is: ', new_node.val)
-                if current.val == val:
+        def insersion(node, val):
+            print('line 57 start to insert val: ', val)
+            if node is None:
+                node = Node(val)
+                print('line 60 root is created: ', node.val)
+                return
+            if node.val < val:
+                print('line 63 node.val is: ', node.val)
+                if node.right is None:
+                    node.right = Node(val)
+                    print('line 66 created node.right: ', node.right.val)
                     return
-                if current.val > val:
-                    print('current will be move to the left: ', new_node.val)
-                    current = current.left
                 else:
-                    print('current will be move to the right: ', new_node.val)
-                    current = current.right
+                    print('line 68 to call insersion and pass in node.right: ', node.right.val)
+                    print('line 69 to call insersion and pass in val: ', val)
+                    insersion(node.right, val)
+                    return
+            elif node.val > val:
+                print('line 72 node.val is: ', node.val)
+                if node.left is None:
+                    node.left = Node(val)
+                    print('line 75 created node.left: ', node.left.val)
+                    return
+                else:
+                    print('line 77 to call insersion and pass in node.left: ', node.left.val)
+                    print('line 78 to call insersion and pass in val: ', val)
+                    insersion(node.left, val)
+                    return
 
-            current = new_node
-            print('current.val is: ', current.val)
-            print('self.root.right.val is: ', self.root.right.val)
+        insersion(self.root, val)
 
 
 
-            # if current.val == val:
-            #     return
-            # elif current.val > val:
-            #     current.left = new_node
-            #     print('new_node is added to the left: ', current.left.val)
-            # else:
-            #     current.right = new_node
-            #     print('new_node is added to the right: ', current.right.val)
-
-
-new_tree = BST([10, 12])
+new_tree = BST([10, 12, 11, 15, 20, 17])
 
 # 10
 print(new_tree.root.val)
 # 12
 print(new_tree.root.right.val)
-# print(new_tree.root.right.left.val)
+# 11
+print(new_tree.root.right.left.val)
+# 15
+print(new_tree.root.right.right.val)
+# 20
+print(new_tree.root.right.right.right.val)
+# 17
+print(new_tree.root.right.right.right.left.val)
 
 
 
