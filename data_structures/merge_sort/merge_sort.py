93ea031432bc0f4fb1fc238402e887cc28864982
Joyce Liao <joyliao07@gmail.com>
2019-01-21 13:45:31 -0800

    start

---
+++ data_structures/merge_sort/merge_sort.py
@@ -0,0 +1,62 @@
+"""This module will contain class Sort and its related methods."""
+
+
+class Sort(object):
+    """
+    """
+    def __init__(self, iterable=None):
+        if iterable is None:
+            iterable = []
+
+        if type(iterable) is not list:
+            raise TypeError('Input is not a list.')
+
+        self.len = len(iterable)
+        self.lst = iterable
+
+    def __repr__(self):
+        output = f'<Input list: { self.lst }>'
+        return output
+
+    def __str__(self):
+        output = f'Input list length is { self.len }'
+        return output
+
+    def selection(self):
+        """
+        """
+        for i in self.lst:
+            if isinstance(i, str):
+                raise TypeError('Items must be integers.') 
+
+        for i in range(len(self.lst)):
+            smallest_idx = i
+            for a in range(i+1, len(self.lst)):
+                if self.lst[smallest_idx] > self.lst[a]:
+                    smallest_idx = a
+
+            self.lst[i], self.lst[smallest_idx] = self.lst[smallest_idx], self.lst[i]
+        return(self.lst)
+
+    def mergeSort(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# inpu = Sort([1, 2, 11, 4, 5, -11])
+
+# inpu.selection()
